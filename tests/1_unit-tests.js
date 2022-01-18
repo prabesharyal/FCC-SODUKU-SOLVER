e7fe0800387ba8381f4f9881c6ab6c0fa8b10f22
Alexalim28 <alexis.ahab@gmail.com>
2022-01-18 16:55:50 +0100

    Completed all routing features and logic controllers

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,34 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Solver = require('../controllers/sudoku-solver.js');
+const Solver = require("../controllers/sudoku-solver.js");
 let solver;
 
-suite('UnitTests', () => {
+suite("UnitTests", () => {
+  suite("Testing puzzle", function () {
+    test("valid puzzle string of 81 characters", function () {});
+    test("puzzle string with invalid characters (not 1-9 or .)", function () {});
+    test("puzzle string that is not 81 characters in length", function () {});
+  });
 
+  suite("Testing row", function () {
+    test("valid row placement", function () {});
+    test("invalid row placement", function () {});
+  });
+
+  suite("Testing column", function () {
+    test("valid column placement", function () {});
+    test("invalid column placement", function () {});
+  });
+
+  suite("Testing region", function () {
+    test("a valid region (3x3 grid) placement", function () {});
+    test("invalid region (3x3 grid) placement", function () {});
+  });
+
+  suite("Testing solver", function () {
+    test("valid puzzle strings pass the solver", function () {});
+    test("invalid puzzle strings fail the solver", function () {});
+    test("solver returns the expected solution for an incomplete puzzle", function () {});
+  });
 });
